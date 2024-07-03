96b61b0572b56fee1bc7f09ec4fb47abec283724
UA83 <alves.enders@gmail.com>
2024-07-03 23:39:55 +0100

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Bells',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Hoewll',
+    username: 'Antto',
+    email: 'antoo@april.biz'
+  },
+  {
+    id: 3,
+    name: 'Romario Silva',
+    username: 'roma9',
+    email: 'roma9@april.biz'
+  },
+  {
+    id: 4,
+    name: 'Bebeto Lima',
+    username: 'bebeto7',
+    email: 'bebeto7@april.biz'
+  },
+]
