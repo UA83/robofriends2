69fa02144ef815b8eabde499cc393158462e6938
UA83 <alves.enders@gmail.com>
2024-07-04 18:05:27 +0100

    change color of card to blue

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Card = ({ id, name, email }) => {
   return (
-    <div className="tc bg-light-yellow dib br3 pa3 ma2 grow bw shadow-2" >
+    <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw shadow-2" >
       <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
